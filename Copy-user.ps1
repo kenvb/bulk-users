88995b94efa74ec712991df48d5dfc4668ab70c0
Ken Vanden Branden <kenvdb@gmail.com>
2016-11-04 16:45:29 +0100

    added try-catch
    Trying to clean up code. Make errors more useful

--- Copy-user.ps1
+++ Copy-user.ps1
@@ -43,45 +43,68 @@ $i++
         #{
             try
             {
-            #$_ gets lost between try and catch. Redefining.
-            $Erroruser = $_
-            #The template account is based on the input of $course and "cursist"
             $CreateTemplate = $Course + "Cursist"
             #Important: specify the properties to be copied.
-            $Template = Get-ADUser -Identity $CreateTemplate -Properties office, department, title, physicaldeliveryofficename
+            $Template = Get-ADUser -Identity $CreateTemplate -Properties office, department, title, physicaldeliveryofficename -ErrorAction Stop
+
+            }
+                catch
+                {
+                $ErrorWarn = $_.exception.message
+                Write-Warning $ErrorWarn
+                }
+            try
+            {
+            #$_ gets lost between try and catch. Redefining.
+            $Erroruser = $_          
             #Copy OU of template account. The regular expression gets rid of unwanted information.
             $ou = $Template.DistinguishedName -replace '^cn=.+?(?!\\),'
             #User account has to be created as disabled
             new-aduser -GivenName $_.GivenName -Surname $_.Surname -Department $_.Department -Title $_.title -Name "$($_.GivenName) $($_.Surname)" -SamAccountName "$($_.GivenName).$($_.Surname)" -UserPrincipalName "$($_.GivenName).$($_.Surname)@$($domain.Forest)" -Instance $($Template) -Path $ou -Description $datum -Enabled:$false -ErrorAction Stop
-            Write-Verbose "$($Erroruser.GivenName) $($Erroruser.Surname) created"
-            "$($Erroruser.GivenName) $($Erroruser.Surname) created" | Out-File c:\Create-user-log.txt -Append
-            get-aduser -Identity $Template -Properties memberof | Select-Object -ExpandProperty memberof | Add-ADGroupMember -Members "$($_.GivenName).$($_.Surname)" -PassThru
+            Write-Verbose "$($_.GivenName) $($_..Surname) created"
+            "$datum : $($_.GivenName) $($_.Surname) created" | Out-File c:\Create-user-log.txt -Append
             }
-            catch
-            {
-            Write-Warning "$($Erroruser.GivenName) $($Erroruser.Surname) Skipping creation: user already exists or OU/rights problem"
-            "$($Erroruser.GivenName) $($Erroruser.Surname) Skipping creation: user already exists or OU/rights problem"| Out-File c:\Create-user-errors.txt -Append
-            #Set a flag to prevent changing an existing user's password.
-            $AlreadyExists = $true
-            }
-                #Set users' password. Unless already exists
-                if (-not($AlreadyExists))
+                catch
                 {
-                Set-ADAccountPassword -Identity "$($_.GivenName).$($_.Surname)" -NewPassword $secpass -Reset
-                Write-Verbose "$($Erroruser.GivenName) $($Erroruser.Surname) password set"
-                #Force a user to change his pw at next logon
+                Write-Warning "$($Erroruser.GivenName) $($Erroruser.Surname) Skipping creation: user already exists or OU/rights problem"
+                "$($Erroruser.GivenName) $($Erroruser.Surname) Skipping creation: user already exists or OU/rights problem"| Out-File c:\Create-user-errors.txt -Append
+                #Set a flag to prevent changing an existing user's password.
+                $AlreadyExists = $true
+                }
+            try
+            {
+            #Make new account also member of template user's groups.
+            get-aduser -Identity $Template -Properties memberof | Select-Object -ExpandProperty memberof | Add-ADGroupMember -Members "$($_.GivenName).$($_.Surname)" -PassThru -ErrorAction stop
+            }
+                catch
+                {
+                $ErrorWarn = $_.exception.message
+                Write-Warning $ErrorWarn + "Can't get groups from template user"
+                }
+                #Set users' password. Unless user already exists
+                    if (-not($AlreadyExists))
+                    {
+                    try
+                    {
+                    Set-ADAccountPassword -Identity "$($_.GivenName).$($_.Surname)" -NewPassword $secpass -Reset
+                    Write-Verbose "$($Erroruser.GivenName) $($Erroruser.Surname) password set"
+                    }
+                        catch
+                        {
+                        $ErrorWarn = $_.exception.message
+                        Write-Warning $ErrorWarn + "Can't set AD PW"
+                        }
                 Set-ADUser -ChangePasswordAtLogon:$true -Identity "$($_.GivenName).$($_.Surname)" -SmartcardLogonRequired:$false
                 }
-                    #Enable users' account unless the Isdisabled switch is used
-                    if(-Not ($Isdisabled))
-                    {
-                    Enable-ADAccount -Identity "$($_.GivenName).$($_.Surname)"
-                    Write-Verbose "$($Erroruser.GivenName) $($Erroruser.Surname) account enabled"
-                    }
-    
-        Set-ADAccountControl -AccountNotDelegated:$false -AllowReversiblePasswordEncryption:$false -CannotChangePassword:$false -DoesNotRequirePreAuth:$false `
-        -Identity "$($_.GivenName).$($_.Surname)" -PasswordNeverExpires:$false -UseDESKeyOnly:$false
-        Write-Verbose "$($Erroruser.GivenName) $($Erroruser.Surname) default security values set"
+#Enable users' account unless the Isdisabled switch is used
+            if(-Not ($Isdisabled))
+            {
+            Enable-ADAccount -Identity "$($_.GivenName).$($_.Surname)"
+            Write-Verbose "$($Erroruser.GivenName) $($Erroruser.Surname) account enabled"
+            }
+            Set-ADAccountControl -AccountNotDelegated:$false -AllowReversiblePasswordEncryption:$false -CannotChangePassword:$false -DoesNotRequirePreAuth:$false `
+            -Identity "$($_.GivenName).$($_.Surname)" -PasswordNeverExpires:$false -UseDESKeyOnly:$false
+            Write-Verbose "$($Erroruser.GivenName) $($Erroruser.Surname) default security values set"
         
         }
         <# else
@@ -92,7 +115,7 @@ $i++
        } 
     #}
 else
-{
-"$Csvlocation does not exist" | Out-File c:\Create-user-errors.txt -Append
-Write-warning "$Csvlocation does not exist"
-}
+    {
+    "$Datum : $Csvlocation does not exist" | Out-File c:\Create-user-errors.txt -Append
+    Write-warning "$Csvlocation does not exist"
+    }
